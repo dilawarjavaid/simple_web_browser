325ee3b03469525d2915711700497c46a838240e
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-05 08:26:10 -0700

    Define Browser class inheriting from QMainWindow

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6.QtCore import QUrl
+
+
+#Creates a Browser class that inherits from QMainWindow, the main window class for PyQt apps.
+class Browser(QMainWindow):
+    def __init__(self):
+        super().__init__()
