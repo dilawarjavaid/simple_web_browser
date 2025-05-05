03a886168bce8074877d36ea71df6f15a9125c29
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-05 08:30:36 -0700

    Set window size and title for browser

--- main.py
+++ main.py
@@ -11,18 +11,25 @@ class Browser(QMainWindow):
         #Add the Web View
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("https://www.google.com"))
+
         # Create Address Bar
         self.address_bar = QLineEdit()
         self.address_bar.setPlaceholderText("Enter URL and press Enter")
         self.address_bar.returnPressed.connect(self.load_url_from_address_bar)
+
         # Layout to hold the address bar and the browser view
         layout = QVBoxLayout()
         layout.addWidget(self.address_bar)
         layout.addWidget(self.browser)
 
+        # Container widget
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+
+        #set window properties
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('Simple Web Browser')
 
 
 
