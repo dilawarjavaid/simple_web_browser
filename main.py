8413d2c7bf184ef9ea940ad7c8c240f9b0ea93b2
Dilawar Javaid <dilawar.javaid864@gmail.com>
2025-05-05 08:25:26 -0700

    Initial import of PyQt6 modules and standard libraries

---
+++ main.py
@@ -0,0 +1,4 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QWidget
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import QUrl
